7edec361766d3b5bc7653e89e6ecab27c9180cc4
Jonathan <jonathancbadger@gmail.com>
2022-05-26 16:35:03 -0500

    Updated docs

--- Sources/Terminus/Color/ColorPalette.swift
+++ Sources/Terminus/Color/ColorPalette.swift
@@ -6,13 +6,9 @@ import Foundation
 
 /**
  The ColorPalette protocol provides a simple set of requirements (two functions) for developing custom color palettes.
- 
- 
- 
- interface that is used by the shared ``Colors`` object to manage  terminal program.  Conformance involves writing two functions, allColors() defaultPair().  The allCollors() returns an array of all colors available in the color palette and defaultPair() returns a default ColorPair to use for drawing text.
  */
 public protocol ColorPalette {
-    ///Returns an array of all colors avaible in the color palette.
+    ///Returns an array of all color names and colors avaible in the color palette.
     func allColors() -> [(name: String, color: Color)]
     ///Returns the default ``ColorPair`` for the palette.
     func defaultPair() -> ColorPair
