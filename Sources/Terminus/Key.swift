56fbbc7986928928c607c08e32ad07cc9256f055
Jonathan <jonathancbadger@gmail.com>
2022-07-07 14:38:55 -0500

    Deleted the old 'KeyCode' enum in favor of more specific enums that encapsulate various types of keyboard keys.

--- Sources/Terminus/Key.swift
+++ Sources/Terminus/Key.swift
@@ -6,55 +6,62 @@ import Foundation
 /**
  A key press as returned from the Terminal ``Terminal/getKey()`` function.
  
- The keyCode property can be useful for programs wishing to organize behavior based on specific forms of input (arrow keys, function keys, etc.)
+ Keys are typically captured from the shared terminal and used for downstream procerssing of user input.  For example:
  
- - Parameters:
-    - rawValue: A String represeting a key press
+ ```swift
+ import Terminus
+ let terminal = Terminal.shared
+ terminal.write("Press enter to break.")
+ while true {
+    if let key = try? terminal.getKey(),
+        key.rawValue == CarriageReturn {
+            break
+    }
+ }
+ ```
+ 
+ > Important:  The `rawValue` for a ``Key``  is a ``String`` and may or may not be represented by a single character.  Escaped characters (navigation keys, control keys, etc.) and copy and paste operations have rawValues of varying length.
+ 
+
  */
 public struct Key {
-    ///A raw string representation of a keypress as captured from the terminal input representation.
+     /**
+      A raw string representation of a keypress as captured from the standard input.
+     */
     public let rawValue: String
-    ///An optional ``KeyCode`` for special keys (arrow keys, function keys, etc.)
-    public private(set) var keyCode: KeyCode? = nil
+    /////An optional ``KeyCode`` for special keys (arrow keys, function keys, etc.)
+    //public private(set) var keyCode: KeyCode? = nil
+   
     
     public init(rawValue: String) {
         self.rawValue = rawValue
-        self.keyCode = KeyCode(rawValue: rawValue)
+        
     }
     
     ///Indicates if the key is one of the arrow keys (up, down, right, or left)
-    public var isArrowKey: Bool {
-        guard let keyCode = keyCode else {
-            return false
-        }
-        let arrowKeys: [KeyCode] = [.KEY_UP, .KEY_DOWN, .KEY_RIGHT, .KEY_LEFT]
-        return arrowKeys.contains(keyCode)
+    var isNavigation: Bool {
+        return NavigationKey(rawValue: rawValue) != nil
     }
-    
     ///Indicates if the key is one of the functions keys (F1-F12)
-    public var isFunctionKey: Bool {
-        guard let keyCode = keyCode else {
-            return false
-        }
-        let functionKeys: [KeyCode] = [.KEY_F1, .KEY_F2, .KEY_F3, .KEY_F4, .KEY_F5,
-                                       .KEY_F6, .KEY_F7, .KEY_F8, .KEY_F9, .KEY_F10, .KEY_F11, .KEY_F12]
-        return functionKeys.contains(keyCode)
+    var isFunction: Bool {
+        return FunctionKey(rawValue: rawValue) != nil
     }
     
-    ///Indicates if the is backspace or delete
-    public var isBackspaceKey: Bool {
-        guard let keyCode = keyCode else {
-            return false
-        }
-        let backSpaceKeys: [KeyCode] = [.KEY_DEL, .KEY_BS]
-        return backSpaceKeys.contains(keyCode)
+    ///Indicates the key is a control key (control, option, or command)
+    var isControl: Bool {
+        return ControlKey(rawValue: rawValue) != nil
     }
     
-    ///Indicates if the key is a printable control key (one of tab, linefeed, or carrage return)
-    public var isPrintableControlKey: Bool {
-        guard let keyCode = keyCode else { return false }
-        let printableKeyCodes: [KeyCode] = [.KEY_TAB, .KEY_LF, .KEY_CR]
-        return printableKeyCodes.contains(keyCode)
-        
+    ///Indicates if the key is a printable character or string of characters
+    var isCharacter: Bool {
+        if isNavigation || isControl || isFunction {
+            return false
+        } else {
+            return true
+        }
     }
 }
+
+
+
+
--- Sources/Terminus/KeyCode.swift
+++ Sources/Terminus/KeyCode.swift
@@ -2,64 +2,92 @@ import Foundation
 
 //https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences#cursor-positioning
 
-///Virtual terminal input keys
-public enum KeyCode: String {
-    /// ASCII control c (^c)
-    case KEY_CONTROL_C = "\u{3}"
-    /// ASCII control d (^d) or end of transmission EOT
-    case KEY_CONTROL_D = "\u{4}"
-    /// ASCII control z (^z)
-    case KEY_CONTROL_Z = "\u{1a}"
-    /// ASCII BS
-    case KEY_BS = "\u{7f}"
-    /// ASCII tab (HT)
-    case KEY_TAB = "\t"
-    /// ASCII Line feed (LF)
-    case KEY_LF = "\n"
-    /// ASCII carriage return (CR)
-    case KEY_CR = "\r"
-    /// ASCII escape (ESC)
-    case KEY_ESC = "\u{1b}"
-    /// ASCII delete (DEL)
-    case KEY_DEL = "\u{1b}[~3"
+/**
+ Keycodes/escape sequences corresponding to the navigation keys (arrow, page up/page down, home/end, or insert/delete) on a standard keyboard.
+ */
+public enum NavigationKey: String, CaseIterable {
     /// down-arrow key
-    case KEY_DOWN = "\u{1b}[B"
+    case down = "\u{1b}[B"
     /// up-arrow key
-    case KEY_UP = "\u{1b}[A"
+    case up = "\u{1b}[A"
     /// left-arrow key
-    case KEY_LEFT = "\u{1b}[D"
+    case left = "\u{1b}[D"
     /// right-arrow key
-    case KEY_RIGHT = "\u{1b}[C"
+    case right = "\u{1b}[C"
     /// home key
-    case KEY_HOME = "\u{1b}[H"
+    case home = "\u{1b}[H"
     /// end key
-    case KEY_END = "\u{1b}[F"
-    /// F1 key
-    case KEY_F1 = "\u{1b}[P"
-    /// F2 key
-    case KEY_F2 = "\u{1b}[Q"
-    /// F3 key
-    case KEY_F3 = "\u{1b}[R"
-    /// F4 key
-    case KEY_F4 = "\u{1b}[S"
-    /// F5 key
-    case KEY_F5 = "\u{1b}[15~"
-    /// F6 key
-    case KEY_F6 = "\u{1b}[17~"
-    /// F7 key
-    case KEY_F7 = "\u{1b}[18~"
-    /// F8 key
-    case KEY_F8 = "\u{1b}[19~"
-    /// F9 key
-    case KEY_F9 = "\u{1b}[20~"
-    /// F10 key
-    case KEY_F10 = "\u{1b}[21~"
-    /// F11 key
-    case KEY_F11 = "\u{1b}[23~"
-    /// F12 key
-    case KEY_F12 = "\u{1b}[24~"
+    case end = "\u{1b}[F"
     /// page up key
-    case KEY_PAGEUP = "\u{1b}[5~"
+    case pageUp = "\u{1b}[5~"
     /// page down key
-    case KEY_PAGEDOWN = "\u{1b}[6~"
+    case pageDown = "\u{1b}[6~"
+    ///Unix-like mapped delete (DEL)
+    case delete = "\u{1b}[~3"
+    ///Insert key
+    case insert = "\u{1b}[~2"
 }
+
+/**
+ Keycodes/escape sequences corresponding to the function keys (F1-F12) on a standard keyboard.
+ */
+public enum FunctionKey: String, CaseIterable {
+    /// F1 key
+    case F1 = "\u{1b}[P"
+    /// F2 key
+    case F2 = "\u{1b}[Q"
+    /// F3 key
+    case F3 = "\u{1b}[R"
+    /// F4 key
+    case F4 = "\u{1b}[S"
+    /// F5 key
+    case F5 = "\u{1b}[15~"
+    /// F6 key
+    case F6 = "\u{1b}[17~"
+    /// F7 key
+    case F7 = "\u{1b}[18~"
+    /// F8 key
+    case F8 = "\u{1b}[19~"
+    /// F9 key
+    case F9 = "\u{1b}[20~"
+    /// F10 key
+    case F10 = "\u{1b}[21~"
+    /// F11 key
+    case F11 = "\u{1b}[23~"
+    /// F12 key
+    case F12 = "\u{1b}[24~"
+}
+
+/**
+ Keycodes/escape sequences corresponding to key sequences beginning with the control key.
+ */
+public enum ControlKey: String {
+    /// ASCII control c (^c)
+    case controlC
+    /// ASCII control d (^d) or end of transmission EOT
+    case controlD
+    /// ASCII control z (^z)
+    case controlZ
+    case other
+    
+    public init?(rawValue: String) {
+        if rawValue.range(of: #"^\^[A-Za-z]$"#, options: .regularExpression) != nil {
+            switch rawValue {
+            case "\u{3}":
+                self = .controlC
+            case "\u{4}":
+                self = .controlD
+            case "\u{1a}":
+                self = .controlZ
+            default:
+                self = .other
+            }
+        } else { return nil }
+    }
+}
+
+
+
+
+
+
