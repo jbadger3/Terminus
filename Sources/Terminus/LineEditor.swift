1bdb218c0a755982a3861f45c28bf29899641131
Jonathan <jonathancbadger@gmail.com>
2022-07-12 11:11:56 -0500

    Refactored LineEditor to use Foundation AttributedString as a buffer...which leads to simplified code and removal of Cell and CellBuffer from the project.

--- Sources/Terminus/LineEditor.swift
+++ Sources/Terminus/LineEditor.swift
@@ -17,11 +17,12 @@ public class LineEditor {
     public typealias ShouldAddKeyToLineBuffer = Bool
     
     var startLocation: Location
-    ///Stores the contents of user input
-    public var cellBuffer: CellBuffer
+    ///Stores the contents of user input as an attributed string.
+    public var buffer: AttributedString
     ///When set to true ends user interaction and returns any caputred input from getLines
     public var shouldEndEditing: Bool = false
     
+    public var cursorDangling: Bool = false
     
     public var navigationKeyHandler: ((Key) -> ShouldWriteBuffer)? = nil
     public var functionKwyHandler: ((Key) -> ShouldWriteBuffer)? = nil
@@ -35,7 +36,7 @@ public class LineEditor {
             throw TerminalError.failedToReadTerminalResponse(message: "Failed to get response for cursor position")
         }
         self.startLocation = startLocation
-        self.cellBuffer = CellBuffer()
+        self.buffer = AttributedString()
         self.navigationKeyHandler = defaultNavigationKeyHandler
         self.characterKeyHandler = defaultCharacterHandler
     }
@@ -56,7 +57,7 @@ public class LineEditor {
             if let key = try? terminal.getKey() {
                 var shouldWriteBuffer: Bool = false
                 if key.isCharacter,
-                   !key.rawValue.starts(with: "\u{1b}["),
+                   !key.rawValue.contains("\u{1b}["),
                    let characterKeyHandler  = characterKeyHandler {
                     let (writeBuffer, shouldAddKeyToLineBuffer) = characterKeyHandler(key)
                     shouldWriteBuffer = writeBuffer
@@ -78,28 +79,34 @@ public class LineEditor {
                 }
             }
         }
-        return cellBuffer.stringValue
+        return String(buffer.characters)
     }
     
     func writeBuffer() {
+        guard let textAreaSize = try? terminal.textAreaSize() else { return }
         let startPosition = terminal.cursor.location
         terminal.cursor.save()
         terminal.cursor.set(visibility: false)
         terminal.cursor.move(toLocation: startLocation)
         
         if let startPosition = startPosition,
-            let lastCellLocation = locationForBufferIndex(cellBuffer.cells.endIndex - 1) {
+           buffer.characters.count > 0,
+           let lastCellLocation = locationForBufferIndex(buffer.characters.index(before: buffer.characters.endIndex)) {
             var currentY = startPosition.y
             while currentY <= lastCellLocation.y {
                 terminal.executeControlSequence(ANSIEscapeCode.eraseToEndOfLine)
+                if currentY == textAreaSize.height {
+                    break
+                }
                 currentY += 1
                 terminal.cursor.move(toLocation: Location(x: 1, y: currentY))
+                
             }
             terminal.cursor.move(toLocation: startLocation)
         }
 
         
-        cellBuffer.writeBuffer()
+        terminal.write(attributedString: buffer)
         let endPosition = terminal.cursor.location
         if startPosition != endPosition {
             terminal.cursor.restore()
@@ -108,22 +115,84 @@ public class LineEditor {
         terminal.cursor.set(visibility: true)
     }
     
-    func locationForBufferIndex(_ index: Int) -> Location? {
+
+    
+    func locationForBufferIndex(_ index: AttributedString.CharacterView.Index) -> Location? {
         guard let textAreaSize = try? terminal.textAreaSize() else { return nil }
-        let startingLocationIndex = (startLocation.y - 1) * textAreaSize.width + startLocation.x - 1
-        let locationIndex = index + startingLocationIndex
-        let y = locationIndex / textAreaSize.width + 1
-        let x = locationIndex % textAreaSize.width + 1
+        
+       
+        if buffer.characters.startIndex == index {
+            return startLocation
+        }
+        
+        guard buffer.characters.indices.contains(index) else { return nil }
+        
+        let subString = String(buffer.characters.prefix(through: index))
+        
+        let pieces = subString.split(separator: Character("\n"))
+        var y = startLocation.y //+ pieces.count - 1
+        var x: Int = 0
+        
+        //count number of wrapping lines
+        for (pieceIdx, piece) in pieces.enumerated() {
+            let xOffset = pieceIdx == 0 ? startLocation.x - 1 : 0
+   
+           
+            y += (piece.count + xOffset) / textAreaSize.width
+            
+            
+            if pieceIdx == pieces.count - 1 {
+                x = (piece.count + xOffset) % textAreaSize.width
+            }
+        }
         return Location(x: x, y: y)
     }
-    
-    func bufferIndexFor(location: Location) -> Int? {
+
+    func bufferIndexFor(location: Location) -> AttributedString.CharacterView.Index? {
         guard let textAreaSize = try? terminal.textAreaSize() else { return nil }
-        let startingLocationIndex = (startLocation.y - 1) * textAreaSize.width + startLocation.x - 1
-        let locationIndex = (location.y - 1) * textAreaSize.width + location.x - 1
+        guard location >= startLocation else { return nil }
+        let textWidth = textAreaSize.width
         
-        return locationIndex - startingLocationIndex
+        var currentY = startLocation.y
+        let pieces = buffer.characters.split(separator: "\n")
+        if pieces.count == 0,
+           currentY == location.y,
+           buffer.characters.distance(from: buffer.characters.startIndex, to: buffer.characters.endIndex) == location.x - startLocation.x {
+            return buffer.characters.endIndex
+        }
+        for (pieceIdx, piece) in pieces.enumerated() {
+            var xOffset = pieceIdx == 0 ? startLocation.x - 1: 0
+            
+            var numCharacters = piece.count + xOffset
+            var currentPieceIndex = piece.startIndex
+            while numCharacters >= textWidth {
+                if currentY == location.y {
+                    if piece.distance(from: currentPieceIndex, to: piece.endIndex) <= 1 {
+                        return piece.endIndex
+                    } else {
+                        return piece.index(currentPieceIndex, offsetBy: location.x - xOffset - 1, limitedBy: piece.endIndex)
+                    }
+                } else {
+                    if let endIndex = piece.index(currentPieceIndex, offsetBy: textWidth - xOffset, limitedBy: piece.endIndex) {
+                        currentPieceIndex = endIndex
+                        numCharacters = piece.distance(from: currentPieceIndex, to: piece.endIndex)
+                        currentY += 1
+                        xOffset = 0
+                    }
+                }
+            }
+            if currentY == location.y {
+                if piece.distance(from: currentPieceIndex, to: piece.endIndex) <= 1 {
+                    return piece.endIndex
+                } else {
+                    return piece.index(currentPieceIndex, offsetBy: location.x - xOffset - 1, limitedBy: piece.endIndex)
+                }
+            }
+            currentY += 1
+        }
+        return nil
     }
+
     
     /**
      
@@ -131,21 +200,21 @@ public class LineEditor {
     public func defaultNavigationKeyHandler(key: Key) -> ShouldWriteBuffer {
         guard let currentLocation = terminal.cursor.location else { return false }
         guard let navigationKey = NavigationKey(rawValue: key.rawValue) else { return false }
-        
         switch navigationKey {
         case .down:
             if let bufferIndex = bufferIndexFor(location: Location(x: currentLocation.x, y: currentLocation.y + 1)),
-               cellBuffer.cells.indices.contains(bufferIndex) {
+               buffer.characters.indices.contains(bufferIndex) {
                 terminal.cursor.move(1, direction: .down)
-            } else if let lastCellLocation = locationForBufferIndex(cellBuffer.cells.count - 1),
+            } else if buffer.characters.count > 0,
+                      let lastCellLocation = locationForBufferIndex(buffer.characters.index(before: buffer.characters.endIndex)),
                       currentLocation.y < lastCellLocation.y {
                 terminal.cursor.move(toLocation: Location(x: lastCellLocation.x + 1, y: lastCellLocation.y))
             }
         case .up:
             if let bufferIndex = bufferIndexFor(location: Location(x: currentLocation.x, y: currentLocation.y - 1)),
-               cellBuffer.cells.indices.contains(bufferIndex) {
+               buffer.characters.indices.contains(bufferIndex) {
                 terminal.cursor.move(1, direction: .up)
-            } else if let firstCellLocation = locationForBufferIndex(0),
+            } else if let firstCellLocation = locationForBufferIndex(buffer.characters.startIndex),
                       currentLocation.y > firstCellLocation.y {
                 terminal.cursor.move(toLocation: firstCellLocation)
             }
@@ -159,7 +228,8 @@ public class LineEditor {
                 }
             }
         case .right:
-            if let lastCellLocation = locationForBufferIndex(cellBuffer.cells.count - 1),
+            if buffer.characters.count > 0,
+                let lastCellLocation = locationForBufferIndex(buffer.characters.index(before: buffer.characters.endIndex)),
                currentLocation != Location(x: lastCellLocation.x + 1, y: lastCellLocation.y),
                let textArea = try? terminal.textAreaSize() {
                 if currentLocation.y < lastCellLocation.y && currentLocation.x == textArea.width {
@@ -170,7 +240,6 @@ public class LineEditor {
             }
         default: ()
         }
-        
         return false
     }
     
@@ -183,37 +252,38 @@ public class LineEditor {
             let shouldWriteBuffer = defaultBackspaceKeyHandler()
             return (shouldWriteBuffer, false)
         }
-        if key.rawValue == CarriageReturn {
+        if key.rawValue == Linefeed {
             shouldEndEditing = true
             return (false, false)
         }
         
-        return (false, false)
+        return (true, true)
     }
     
     func defaultBackspaceKeyHandler() -> ShouldWriteBuffer {
-        if cellBuffer.cells.count > 0,
+        if buffer.characters.count > 0,
            let currentLocation = terminal.cursor.location,
-           let currentIndex = bufferIndexFor(location: currentLocation) {
-            let deleteIndex = currentIndex - 1
-            if deleteIndex >= 0 {
-                cellBuffer.deleteCell(index: deleteIndex)
-                if currentLocation.x > 1 {
-                    terminal.cursor.move(toLocation: Location(x: currentLocation.x - 1, y: currentLocation.y))
-                    terminal.write("\u{7f}")
-                    terminal.cursor.move(toLocation: Location(x: currentLocation.x - 1, y: currentLocation.y))
-                    
-                } else if currentLocation.y > 1,
-                          let textArea = try? terminal.textAreaSize() {
-                    terminal.cursor.move(toLocation: Location(x: textArea.width, y: currentLocation.y - 1))
-                    terminal.write("\u{7f}")
-                    terminal.cursor.move(toLocation: Location(x: textArea.width, y: currentLocation.y - 1))
-                }
-                return true
+           let currentIndex = bufferIndexFor(location: currentLocation),
+           currentIndex > buffer.characters.startIndex {
+            let deleteIndex = buffer.characters.index(before: currentIndex)
+            
+            buffer.characters.remove(at: deleteIndex)
+            if currentLocation.x > 1 {
+                terminal.cursor.move(toLocation: Location(x: currentLocation.x - 1, y: currentLocation.y))
+                terminal.write("\u{7f}")
+                terminal.cursor.move(toLocation: Location(x: currentLocation.x - 1, y: currentLocation.y))
+                
+            } else if currentLocation.y > 1,
+                      let textArea = try? terminal.textAreaSize() {
+                terminal.cursor.move(toLocation: Location(x: textArea.width, y: currentLocation.y - 1))
+                terminal.write("\u{7f}")
+                terminal.cursor.move(toLocation: Location(x: textArea.width, y: currentLocation.y - 1))
             }
+            return true
         }
-        return false
-    }
+    
+    return false
+}
     
 
     
@@ -227,17 +297,25 @@ public class LineEditor {
             for char in key.rawValue {
                 terminal.write(" ")
                 if let endWriteLocation = terminal.cursor.location {
-                    let cell = Cell(contents: Character(extendedGraphemeClusterLiteral: char), attributes: attributes)
+
                     /*if the cursor is at the last position it will scroll, so the start location needs to be updated*/
                     if startWriteLocation == Location(x: textAreaSize.width, y: textAreaSize.height) && endWriteLocation != startWriteLocation {
                         startLocation = Location(x: startLocation.x, y: startLocation.y - 1)
                     }
-                    if let index = bufferIndexFor(location: endWriteLocation) {
+                    
+                    if let index = bufferIndexFor(location: startWriteLocation) {
                         if startWriteLocation == endWriteLocation {
-                            cellBuffer.add(cell: cell, index: index)
+                            if let nextIndex = bufferIndexFor(location: endWriteLocation) {
+                            buffer.insert(AttributedString(String(char)), at: nextIndex)
+                            }
+                            
+                            
                         } else {
-                            cellBuffer.add(cell: cell, index: index - 1)
+                            buffer.insert(AttributedString(String(char)), at: index)
                         }
+                    } else {
+                        terminal.cursor.move(toLocation: startWriteLocation)
+                        break
                     }
                 } else {
                     terminal.cursor.move(toLocation: startWriteLocation)
